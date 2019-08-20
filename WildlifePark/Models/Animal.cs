5e92a4f4c35af7fe88f7928730a9882761b9c932
Student <example@example.com>
2019-08-20 10:31:56 -0700

    add validation

--- WildlifePark/Models/Animal.cs
+++ WildlifePark/Models/Animal.cs
@@ -8,9 +8,16 @@ namespace WildlifePark.Models
     {
         [Key]
         public int AnimalId { get; set; }
+        [Required]
         public string Species { get; set; }
+        [Required]
+        [StringLength(20)]
         public string Name { get; set; }
+        [Required]
+        [Range(0, 100,
+            ErrorMessage = "Age must be between 0 and 100.")]
         public int Age { get; set; }
+        [Required]
         public string Gender { get; set; }
     }
 }
